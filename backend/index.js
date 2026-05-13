fc9c139afd112b67abb8bf420c54100fb2d0aaaf
Imansa Sandil <imansasandil@gmail.com>
2026-05-13 09:27:00 +0000

    Configure CORS and JSON middleware

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`HSD Backend listening on port ${port}`);
 });
