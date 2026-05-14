5f0876027bf583995af5c43b069bdd028969623a
Imansa Sandil <imansasandil@gmail.com>
2026-05-14 03:37:00 +0000

    Setup mock tracking database object

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+// Dummy tracking data
+const trackingData = {};
+
 app.listen(port, () => {
   console.log(`HSD Backend listening on port ${port}`);
 });
