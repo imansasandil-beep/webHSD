64c9a533e1f625dbd85907ef8ec7e5b5e3bcbc4e
Imansa Sandil <imansasandil@gmail.com>
2026-05-14 12:47:00 +0000

    Add result, loading, and error states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react"
 
 function App() {
   const [trackingNumber, setTrackingNumber] = useState("")
+  const [result, setResult] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
   return (
     <div className="container">
       Hello World
